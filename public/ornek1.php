364365d4445d8965fb1db72b22a43b13d99648a7
Adınız Soyadınız <github-emailiniz@example.com>
2025-10-19 22:32:52 +0300

    ornek1dodya

---
+++ public/ornek1.php
@@ -0,0 +1,5 @@
+<?php
+// ornek1.php test dosyası
+
+echo '<h1 style="color:blue;text-align:center;">Sistem Testi Başarılı! (ornek1.php)</h1>';
+echo '<p style="text-align:center;">Bu dosya otomatik olarak cPanel sunucusuna yüklenmelidir.</p>';
