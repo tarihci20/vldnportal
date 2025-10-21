8fb77b38a5e213faa6b11807f414338e6fc7be09
Adınız Soyadınız <github-emailiniz@example.com>
2025-10-21 20:45:47 +0300

    style: Remove 'boş bırakılabilir' text, add phone field maxlength and placeholder

--- app/views/students/simple-create.php
+++ app/views/students/simple-create.php
@@ -40,7 +40,7 @@
             <!-- Row 3: Sınıf -->
             <div class="mb-4">
                 <label class="block text-sm font-medium text-gray-700 mb-1">Sınıf</label>
-                <input type="text" name="class" class="w-full px-3 py-2 border border-gray-300 rounded" placeholder="Örn: 9-A, 10-B, vb (boş bırakılabilir)">
+                <input type="text" name="class" class="w-full px-3 py-2 border border-gray-300 rounded" placeholder="Örn: 9-A, 10-B, vb">
             </div>
             
             <hr class="my-6">
@@ -54,7 +54,7 @@
                 </div>
                 <div>
                     <label class="block text-sm font-medium text-gray-700 mb-1">Baba Telefonu</label>
-                    <input type="tel" name="father_phone" class="w-full px-3 py-2 border border-gray-300 rounded">
+                    <input type="tel" name="father_phone" maxlength="11" class="w-full px-3 py-2 border border-gray-300 rounded" placeholder="05055974225">
                 </div>
             </div>
             
@@ -69,7 +69,7 @@
                 </div>
                 <div>
                     <label class="block text-sm font-medium text-gray-700 mb-1">Anne Telefonu</label>
-                    <input type="tel" name="mother_phone" class="w-full px-3 py-2 border border-gray-300 rounded">
+                    <input type="tel" name="mother_phone" maxlength="11" class="w-full px-3 py-2 border border-gray-300 rounded" placeholder="05055974225">
                 </div>
             </div>
             
@@ -84,7 +84,7 @@
                 </div>
                 <div>
                     <label class="block text-sm font-medium text-gray-700 mb-1">Öğretmen Telefonu</label>
-                    <input type="tel" name="teacher_phone" class="w-full px-3 py-2 border border-gray-300 rounded">
+                    <input type="tel" name="teacher_phone" maxlength="11" class="w-full px-3 py-2 border border-gray-300 rounded" placeholder="05055974225">
                 </div>
             </div>
             
