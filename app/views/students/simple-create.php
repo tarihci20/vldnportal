9fb60d46770c8b065c78e10d2374c462f6baea57
Adınız Soyadınız <github-emailiniz@example.com>
2025-10-21 20:49:56 +0300

    style: Update phone field placeholder to 05321234567

--- app/views/students/simple-create.php
+++ app/views/students/simple-create.php
@@ -54,7 +54,7 @@
                 </div>
                 <div>
                     <label class="block text-sm font-medium text-gray-700 mb-1">Baba Telefonu</label>
-                    <input type="tel" name="father_phone" maxlength="11" class="w-full px-3 py-2 border border-gray-300 rounded" placeholder="05055974225">
+                    <input type="tel" name="father_phone" maxlength="11" class="w-full px-3 py-2 border border-gray-300 rounded" placeholder="05321234567">
                 </div>
             </div>
             
@@ -69,7 +69,7 @@
                 </div>
                 <div>
                     <label class="block text-sm font-medium text-gray-700 mb-1">Anne Telefonu</label>
-                    <input type="tel" name="mother_phone" maxlength="11" class="w-full px-3 py-2 border border-gray-300 rounded" placeholder="05055974225">
+                    <input type="tel" name="mother_phone" maxlength="11" class="w-full px-3 py-2 border border-gray-300 rounded" placeholder="05321234567">
                 </div>
             </div>
             
@@ -84,7 +84,7 @@
                 </div>
                 <div>
                     <label class="block text-sm font-medium text-gray-700 mb-1">Öğretmen Telefonu</label>
-                    <input type="tel" name="teacher_phone" maxlength="11" class="w-full px-3 py-2 border border-gray-300 rounded" placeholder="05055974225">
+                    <input type="tel" name="teacher_phone" maxlength="11" class="w-full px-3 py-2 border border-gray-300 rounded" placeholder="05321234567">
                 </div>
             </div>
             
